9e08e28e32c50aae0e0e2a472094f70bf5e836b1
Yarik <kbenzema044@gmail.com>
2021-04-02 20:10:53 +0300

    Первая практика

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("Hello");
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели');
+let firstFilm = prompt('Один из поседних просмотренных фильмов');
+let secondFilm = prompt('Один из поседних просмотренных фильмов');
+let firstAppraisal = prompt('На сколько оцените его');
+let secondAppraisal = prompt('На сколько оцените его');
+
+let personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMoviesDB.movies[firstFilm] = firstAppraisal;
+personalMoviesDB.movies[secondFilm] = secondAppraisal;
+console.log(personalMoviesDB);
+
